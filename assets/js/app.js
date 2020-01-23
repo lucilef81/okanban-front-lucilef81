1e7800d09fdac2e4a4dda05c1df6bf6b653b5b1b
jpruliere <jean@oclock.io>
2020-01-23 09:34:00 +0100

    correction atelier 1 - part 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,54 @@ var app = {
 
   // fonction d'initialisation, lancée au chargement de la page
   init: function () {
-    console.log('app.init !');
+    app.addListenersToActions();
+  },
+
+  addListenersToActions: () => {
+    let addListButton = document.getElementById('addListButton');
+    addListButton.addEventListener('click', app.showAddListModal);
+
+    let closeModalButtons = document.querySelectorAll('.close');
+    for (let button of closeModalButtons) {
+      button.addEventListener('click', app.hideModals);
+    }
+
+    let addListForm = document.querySelector('#addListModal form');
+    addListForm.addEventListener('submit', app.handleAddListForm);
+  },
+
+  showAddListModal: () => {
+    let modal = document.getElementById('addListModal');
+    modal.classList.add('is-active');
+  },
+
+  hideModals: () => {
+    let modals = document.querySelectorAll('.modal');
+    for (let modal of modals) {
+      modal.classList.remove('is-active');
+    }
+  },
+
+  handleAddListForm: (event) => {
+    // empêcher d'envoyer vraiment le formulaire (ça rechargerait la page)
+    event.preventDefault();
+
+    let data = new FormData(event.target);
+
+    // ici, pour l'instant, on ne fait rien
+    // mais bientôt, on appellera notre API pour enregistrer la nouvelle liste
+
+    // on la codera plus tard mais on l'appelle tout de suite
+    // cette méthode va créer une nouvelle liste
+    // et pour pouvoir créer une nouvelle liste, il lui faut un nom
+    // ça tombe bien, on vient d'en saisir un dans le formulaire
+    app.makeListInDOM(data.get('name'));
+
+    app.hideModals();
+  },
+
+  makeListInDOM: (listName) => {
+    console.log('Ici, dans le turfu, on créera une liste qui s\'appelle ' + listName);
   }
 
 };
